4dad9ce3e42e9f4116a13ce0f8a859f900aa6554
Aakash <vermaaakash1000@gmail.com>
2022-11-02 13:46:47 +0530

    Deploy script added for cryptoDevs

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -5,15 +5,18 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const { ethers } = require("hardhat");
-
+const {METADATA_URL, WHITELIST_CONTRACT_ADDRESS} =require("../../my-next-app/constants")
 async function main() {
+
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  const metadataUrl = METADATA_URL;
   /* A contract factory in ether.js is a abstraction used to deploy new smart contracts 
  so WhiteListContract here is a factory for instances of our Whitelist Contract*/
-  const WhiteListContractFactory = await ethers.getContractFactory("Whitelist");
-  const deployedWhitelistContract = await WhiteListContractFactory.deploy(10);
-  await deployedWhitelistContract.deployed();
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(metadataUrl,whitelistContract);
+
   console.log(
-    `deployed whitelist Contract at: ${deployedWhitelistContract.address}`
+    `deployed Crypto Devs at: ${deployedCryptoDevsContract.address}`
   );
 }
 
