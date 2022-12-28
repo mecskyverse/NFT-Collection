a6de7effe9983b664b7b0413d6f73684bf599d1f
Aakash <vermaaakash1000@gmail.com>
2022-12-28 14:58:19 +0530

    API endpoint added

---
+++ my-next-app/pages/api/[tokenId].js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+  const name = `Crypto Dev #${tokenId}`
+  const description = "Crypto Devs is an NFT collection for Web3 Developers";
+  const image = `https://github.com/LearnWeb3DAO/NFT-Collection/tree/main/my-app/public/cryptodevs/${Number(tokenId) -1}.svg`;  
+ 
+  return res.json({
+    name :name,
+    description : description,
+    image:image,
+  })
+}
